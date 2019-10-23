0a50fafbd071c16adbc3f7e86cca04723a6134ae
Giampaolo1 <giampaolo.r@outlook.com>
2019-10-23 16:15:09 +0200

    bo

--- script/main.js
+++ script/main.js
@@ -23,11 +23,11 @@ lista.push(cognomeutente)
 for (var index = 0; index < lista.length; index++){
 }
 // cognomeutente posizione prima di sort
-console.log(cognomeutente,index);
+console.log(cognomeutente,[index]);
 
 // e stampa la lista ordinata alfabeticamente.
 lista.sort();
-console.log(cognomeutente,index);
+console.log(cognomeutente,[index]);
 
 
 // console.log(lista, cognomeutente, cognomeutente.length);
