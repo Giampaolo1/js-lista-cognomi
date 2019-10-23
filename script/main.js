c26c08fe60a0afda90fbf014cad71bdafddd581e
Giampaolo1 <giampaolo.r@outlook.com>
2019-10-23 19:01:31 +0200

    impossible

--- script/main.js
+++ script/main.js
@@ -19,13 +19,17 @@ var cognomeutente = "Rossiiiiii";
 // inseriscilo in un array con altri cognomi
 lista.push(cognomeutente)
 
-console.log(lista);
+console.log(lista, "lista dopo push");
 
 // apro ciclo per assegnare la posizione con index
-var posizione
+var classifica;
 for (var index = 0; index < lista.length; index++){
-posizione = index
-}
+  var posizione = lista[index];
+  if (cognomeutente == posizione) {
+    classifica = index;
+    console.log('posizione mio cognome', classifica);
+  }
+// }
 
 // cognomeutente posizione prima di sort
 // console.log(cognomeutente,[index]);
@@ -48,3 +52,36 @@ posizione = index
 //
 // for (var index = 0; index < lista.length; index++){
 // }
+
+
+// DESCRIZIONE:
+// Chiedi all’utente il cognome,
+// inseriscilo in un array con altri cognomi
+// e stampa la lista ordinata alfabeticamente.
+// Scrivi anche la posizione della lista in cui il nuovo utente si trova
+// QUINDI OUTPUT sarà:
+// - array disordinato di partenza
+// - array ordinato;
+// -  posizione in cui si trova il cognome dell’utente nell’array ordinato.
+// // Lista di cognomi già presenti
+// var lista = ["Azzopardi", "Zanardi", "Bianchi", "Verdi","Diawara"];
+// // console.log(lista);
+// // Chiedi all’utente il cognome,
+// var cognomeutente = "Rossiiiiii";
+// // inseriscilo in un array con altri cognomi
+// lista.push(cognomeutente)
+// // console.log(lista);
+// lista.sort();
+// // console.log(lista);
+
+// // apro ciclo per assegnare la posizione con index
+// var posizione;
+// for (var index = 0; index < lista.length; index++){
+//  var elemento = lista[index];
+//  console.log(cognomeutente, elemento);
+//  if(cognomeutente == elemento){
+//    posizione = index;
+//    console.log('posizione mio cognome', posizione);
+//  }
+// }
+// document.getElementById('posizione').innerHTML = lista[posizione] + ' è in posizione ' + posizione;
